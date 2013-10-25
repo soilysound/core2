3c36897f6ed09733e9f5b579b3fbcedd28dd4ad2
Groak, Mark <Mark.Groak@bskyb.com>
2013-10-25 11:15:19 +0100

    Modified the breakpoint script

--- js/head.js
+++ js/head.js
@@ -322,8 +322,6 @@ String.prototype.toCamel = function(){
     var bp = window.getComputedStyle(document.head, null).getPropertyValue('font-family');
     bp = parseInt(bp.replace(/['"]/g, ''), 10);
 
-    document.documentElement.id = ('current-breakpoint-' + bp);
-
     return bp;
   }
 
@@ -352,34 +350,85 @@ String.prototype.toCamel = function(){
     return throttled;
   }
 
-  //check for event listeners to avoid IE8 errors
-  if(window.addEventListener){
 
-    //create a custom breakpoint event
-    var bpEvent = document.createEvent('Event');
-    bpEvent.initEvent('breakPointChange', true, true);
-    bpEvent.currentBreakPoint = getBreakPoint;
-
-    //set current breakpoint on load
-    document.currentBreakPoint = getBreakPoint();
-
-    //add throttled resize listener to check current breakpoint
-    window.addEventListener('resize', throttle(function(){
-
-      var currBP = getBreakPoint();
-
-      //check if the breakpoint has changed and dispatch our custom event
-      if(currBP !== document.currentBreakPoint) {
-
-        document.currentBreakPoint = currBP;
-
-        //trigger our custom event listener to fire
-        document.dispatchEvent(bpEvent);
-      }
-
-    }, 300), false);
+  function dispatchEvent(){
+    console.log('uses match media listener');
+    document.dispatchEvent(bpEvent);
 
   }
+
+
+  if(window.addEventListener){
+
+    /**
+     * Use 2 methods for getting media query feedback -
+     * 1 - If available, use the native matchMedia addListener callback
+     * 2 - Else read the media query from the css
+     */
+
+    //create custom event
+    var bpEvent = document.createEvent('Event');
+    bpEvent.initEvent('breakPointChange', true, true);
+
+    //set current breakpoint
+    document.currentBreakPoint = getBreakPoint();
+
+    /* 1 */
+    if(window.matchMedia && window.matchMedia('all').addListener){
+
+      //bp30
+      window.matchMedia('(max-width:600px)').addListener(function(query){
+
+        if(query.matches){
+          document.currentBreakPoint = 30;
+          window.requestAnimationFrame(dispatchEvent);
+        }
+
+      });
+
+      //bp20
+      window.matchMedia('(min-width:601px) and (max-width:800px)').addListener(function(query){
+
+        if(query.matches){
+          document.currentBreakPoint = 20;
+          window.requestAnimationFrame(dispatchEvent);
+        }
+
+      });
+
+      //bp10
+      window.matchMedia('(min-width:801px)').addListener(function(query){
+
+        if(query.matches){
+          document.currentBreakPoint = 10;
+          window.requestAnimationFrame(dispatchEvent);
+        }
+
+      });
+
+    }
+
+    /* 2 */
+    else {
+
+      window.addEventListener('resize', throttle(function(){
+
+        var currBP = getBreakPoint();
+
+        //check if the breakpoint has changed and dispatch our custom event
+        if(currBP !== document.currentBreakPoint) {
+
+          document.currentBreakPoint = currBP;
+          console.log('uses resize event');
+          //trigger our custom event listener to fire
+          document.dispatchEvent(bpEvent);
+        }
+
+      }, 300), false);
+    }
+
+  }
+
 
 })(window, document);
 
